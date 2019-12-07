b3fb42181e5664e93e64a17b05ca04473ee11d8e
Andrey <a.ka144@mail.ru>
2019-12-07 04:22:56 +0300

    Отделил данные от разметки

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-const MOVIES_COUNT = 5;
-const TOP_MOVIES_COUNT = 2;
+const MOVIES_COUNT = 15;
+const SHOWING_MOVIES_COUNT_ON_START = 5;
+const SHOWING_MOVIES_COUNT_BY_BUTTON = 5;
 
 import {createBoardTemplate} from './components/board.js';
 import {createFilmCardTemplate} from './components/film-card.js';
@@ -7,6 +8,9 @@ import {createShowMoreButtonTemplate} from './components/show-more-button.js';
 import {createSiteMenuTemplate} from './components/site-menu.js';
 import {createUserProfileTemplate} from './components/user-profile.js';
 import {createFilmDetailsTemplate} from './components/film-details.js';
+import {generateMovies} from './mock/movie.js';
+import {movieCount} from './mock/user-profile.js';
+import {generateFilters} from './mock/menu.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -15,29 +19,81 @@ const render = (container, template, place) => {
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 
-render(siteHeaderElement, createUserProfileTemplate(), `beforeend`);
-render(siteMainElement, createSiteMenuTemplate(), `beforeend`);
+render(siteHeaderElement, createUserProfileTemplate(movieCount), `beforeend`);
+
+const movies = generateMovies(MOVIES_COUNT);
+const filters = generateFilters(movies);
+render(siteMainElement, createSiteMenuTemplate(filters), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
+
+const footerStatistics = document.querySelector(`.footer__statistics`);
+footerStatistics.querySelector(`p`).textContent = `${movies.length} movies inside`;
 
 const filmsListElement = siteMainElement.querySelector(`.films-list__container`);
 
-for (let i = 0; i < MOVIES_COUNT; i++) {
-  render(filmsListElement, createFilmCardTemplate(), `beforeend`);
+let showingMoviesCount = SHOWING_MOVIES_COUNT_ON_START;
+for (let i = 0; i < showingMoviesCount; i++) {
+  render(filmsListElement, createFilmCardTemplate(movies[i]), `beforeend`);
 }
 
 render(filmsListElement, createShowMoreButtonTemplate(), `afterend`);
+
+
+const showMoreButton = siteMainElement.querySelector(`.films-list__show-more`);
+showMoreButton.addEventListener(`click`, () => {
+  const prevMoviesCount = showingMoviesCount;
+  showingMoviesCount += SHOWING_MOVIES_COUNT_BY_BUTTON;
+
+  if (showingMoviesCount >= movies.length) {
+    showingMoviesCount = movies.length;
+    showMoreButton.remove();
+  }
+
+  for (let i = prevMoviesCount; i < showingMoviesCount; i++) {
+    render(filmsListElement, createFilmCardTemplate(movies[i]), `beforeend`);
+  }
+});
 
 const filmsListExtraElements = siteMainElement.querySelectorAll(`.films-list--extra`);
 const topRatedFilmsListElement = filmsListExtraElements[0].querySelector(`.films-list__container`);
 const mostCommentedFilmsListElement = filmsListExtraElements[1].querySelector(`.films-list__container`);
 
-for (let i = 0; i < TOP_MOVIES_COUNT; i++) {
-  render(topRatedFilmsListElement, createFilmCardTemplate(), `beforeend`);
+const topRatedMovies = movies[0].rating < movies[1].rating ? [movies[0], movies[1]] : [movies[1], movies[0]];
+movies.slice(2).forEach((movie) => {
+  if (movie.rating > topRatedMovies[0].rating) {
+    if (movie.rating > topRatedMovies[1].rating) {
+      topRatedMovies[0] = topRatedMovies[1];
+      topRatedMovies[1] = movie;
+    } else {
+      topRatedMovies[0] = movie;
+    }
+  }
+});
+
+if (topRatedMovies[1].rating !== 0) {
+  topRatedMovies.forEach(
+      (movie) => render(topRatedFilmsListElement, createFilmCardTemplate(movie), `beforeend`)
+  );
 }
 
-for (let i = 0; i < TOP_MOVIES_COUNT; i++) {
-  render(mostCommentedFilmsListElement, createFilmCardTemplate(), `beforeend`);
+const mostCommentedMovies = movies[0].comments.length < movies[1].comments.length ? [movies[0], movies[1]] : [movies[1], movies[0]];
+movies.slice(2).forEach((movie) => {
+  if (movie.comments.length > mostCommentedMovies[0].comments.length) {
+    if (movie.comments.length > mostCommentedMovies[1].comments.length) {
+      mostCommentedMovies[0] = mostCommentedMovies[1];
+      mostCommentedMovies[1] = movie;
+    } else {
+      mostCommentedMovies[0] = movie;
+    }
+  }
+});
+
+if (topRatedMovies[1].comments.length !== 0) {
+  mostCommentedMovies.forEach(
+      (movie) => render(mostCommentedFilmsListElement, createFilmCardTemplate(movie), `beforeend`)
+  );
 }
 
-render(document.body, createFilmDetailsTemplate(), `beforeend`);
+render(document.body, createFilmDetailsTemplate(movies[0]), `beforeend`);
+
 
---
+++ src/mock/movie.js
@@ -0,0 +1,168 @@
+const TITLES = [
+  `The Shawshank Redemption`,
+  `The Godfather`,
+  `The Dark Knight`,
+  `The Lord of the Rings`,
+  `Pulp Fiction`,
+  `Schindler's List`,
+  `12 Angry Men`,
+  `Inception`,
+  `Fight Club`,
+  `Forrest Gump`,
+  `Joker`,
+  `The Matrix`,
+  `Goodfellas`,
+  `Star Wars`,
+  `One Flew Over the Cuckoo's Nest`
+];
+
+const POSTERS = [
+  `made-for-each-other.png`,
+  `popeye-meets-sinbad.png`,
+  `sagebrush-trail.jpg`,
+  `santa-claus-conquers-the-martians.jpg`,
+  `the-dance-of-life.jpg`,
+  `the-great-flamarion.jpg`,
+  `the-man-with-the-golden-arm.jpg`
+];
+
+const GENRES = [
+  `Action`,
+  `Animation`,
+  `Comedy`,
+  `Crime`,
+  `Drama`
+];
+
+const DIRECTORS = [
+  `David Lynch`,
+  `Martin Scorsese`,
+  `Steven Soderbergh`,
+  `Terrence Malick`,
+  `Abbas Kiarostami`
+];
+
+const WRITERS = [
+  `Stephen King, Frank Darabont`,
+  `Mario Puzo, Francis Ford Coppola`,
+  `Reginald Rose, Reginald Rose`,
+  `Thomas Keneally, Steven Zaillian`,
+  `Chuck Palahniuk, Jim Uhls`
+];
+
+const CASTS = [
+  `Brad Pitt, Edward Norton, Meat Loaf`,
+  `John Travolta, Uma Thurman, Samuel L. Jackson`,
+  `Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page`,
+  `Mark Hamill, Harrison Ford, Carrie Fisher`,
+  `Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss`
+];
+
+const DATES = [
+  `30 March 1945`,
+  `5 September 1978`,
+  `15 November 1991`,
+  `14 March 1999`,
+  `25 August 2007 `
+];
+
+const COUNTRIES = [
+  `United States`,
+  `Great Britain`,
+  `Italy`,
+  `Germany`,
+  `France`
+];
+
+const AGE_RATINGS = [
+  `0+`,
+  `12+`,
+  `16+`,
+  `18+`
+];
+
+const COMMENTS = [{
+  name: `Tim Macoveev`,
+  text: `Interesting setting and a good cast`,
+  time: `2019/12/31 23:59`,
+  emoji: `./images/emoji/smile.png`
+}, {
+  name: `John Doe`,
+  text: `Booooooooooring`,
+  time: `2 days ago`,
+  emoji: `./images/emoji/sleeping.png`
+}, {
+  name: `John Doe`,
+  text: `Very very old. Meh`,
+  time: `2 days ago`,
+  emoji: `./images/emoji/puke.png`
+}];
+
+const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus`;
+const sentenses = text.split(`. `);
+
+const getRandomArrayItem = (array) => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+
+  return array[randomIndex];
+};
+
+const getDescription = () => {
+  let randomSentenses = [];
+  const sentensesCount = Math.floor(Math.random() * 3) + 1;
+
+  for (let i = 0; i < sentensesCount; i++) {
+    randomSentenses.push(getRandomArrayItem(sentenses));
+  }
+
+  return randomSentenses.join(`. `);
+};
+
+const getGenres = (genres) => {
+  const result = [];
+
+  for (let i = 0; i < 3; i++) {
+    result.push(getRandomArrayItem(genres));
+  }
+
+  return result;
+};
+
+const generateMovie = () => {
+  const title = getRandomArrayItem(TITLES);
+
+  return {
+    title,
+    poster: `./images/posters/${getRandomArrayItem(POSTERS)}`,
+    description: getDescription(),
+    rating: Math.floor(Math.random() * 101) / 10.0,
+    year: Math.floor(Math.random() * (2010 - 1950)) + 1950,
+    duration: `${Math.floor(Math.random() * 2) + 1}h ${Math.floor(Math.random() * 60)}m`,
+    genres: getGenres(GENRES),
+    isWatchlist: Math.random() < 0.5,
+    isHistory: Math.random() < 0.1,
+    isFavorite: Math.random() < 0.3,
+    originalTitle: title,
+    userRating: Math.floor(Math.random() * 11),
+    director: getRandomArrayItem(DIRECTORS),
+    writers: getRandomArrayItem(WRITERS),
+    cast: getRandomArrayItem(CASTS),
+    releaseDate: getRandomArrayItem(DATES),
+    country: getRandomArrayItem(COUNTRIES),
+    ageRating: getRandomArrayItem(AGE_RATINGS),
+    comments: COMMENTS.slice(0, Math.floor(Math.random() * 4))
+  };
+};
+
+const generateMovies = (count) => {
+  const movies = [];
+
+  for (let i = 0; i < count; i++) {
+    movies.push(generateMovie());
+  }
+
+  return movies;
+};
+
+export {generateMovies};
+
